a6e3c35fdae4848b6d578fcd517ac119b928fc7b
Julia <us.julija@gmail.com>
2018-04-14 15:21:47 +0300

    Game section with flipping and matchng cards is completed

--- app.js
+++ app.js
@@ -35,9 +35,23 @@ const buttonsArray = [
 ];
 
 // Dublicates buttonsArray and makes one full viariable with 8 pairs of buttons
-let gameButtonsFull = buttonsArray.concat(buttonsArray);
+let fullButtonsArray = buttonsArray.concat(buttonsArray);
 
-// Shuffle function from http://stackoverflow.com/a/2450976
+// My variables
+let clicksCount = 0;
+let firstGuess = '';
+let secondGuess = '';
+let previousButton = null;
+let delay = 1000;
+
+// Id "game" is targeted in HTML, that's the place where the game will be created
+const game = document.getElementById('game');
+const cards = document.createElement('section'); // Section with cards (class="cards") is added
+cards.setAttribute('class', 'cards');
+game.appendChild(cards);
+
+//My functions
+// @description: Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -48,31 +62,84 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
 }
 
-gameButtonsFull = shuffle(gameButtonsFull);
-
-
-// Id "game" is targeted in HTML, that's the place where the game will be created
-const game = document.getElementById('game');
-
-// section with cards (class="cards") is added
-const cards = document.createElement('section');
-cards.setAttribute('class', 'cards');
-game.appendChild(cards); // Appends the created cards section to the game div as a child
+fullButtonsArray = shuffle(fullButtonsArray); // Runs shuffle function for the created array
 
 /*@description: in order to add pictures in divs to the created section
-of cards the code loopes over each item in buttonsArray*/
-gameButtonsFull.forEach(item => {
+of cards the code loopes over each item in fullButtonsArray array*/
+fullButtonsArray.forEach(item => {
   const button = document.createElement('div');   // Creates a div
   button.classList.add('button'); // Applies a button class to the created div
   button.dataset.name = item.name; //Applies data-name to the created div
-  const buttonImage = document.createElement('div'); // Creates one more div
+
+  const front = document.createElement('div'); // Creates div with class front
+  front.classList.add('front')
+
+  const buttonImage = document.createElement('div'); // Creates next div
   buttonImage.classList.add('button_image'); // Applies button_image class to the created div
   buttonImage.style.backgroundImage = `url(${item.img})`; // Adds the background-image to the second div
 
   cards.appendChild(button); // Appends the <div class="button"></div> to the <div class="cards"></div>
+  button.appendChild(front);
   button.appendChild(buttonImage); // Appends the <div class="button_image"></div> to the <div class="button"></div>
+});
+
+/*@description: adds class "match" to the div with class "selected"*/
+const match = () => {
+  const selected = document.querySelectorAll('.selected');
+  selected.forEach(button => {
+    button.classList.add('match');
+  });
+};
+
+const resetGuesses = () => {
+  firstGuess = '';
+  secondGuess = '';
+  clicksCount = 0;
+  previousButton = null;
+
+  const selected = document.querySelectorAll('.selected');
+  selected.forEach(button => {
+    button.classList.remove('selected');
+  });
+};
+
+/*@description: event listener is applied for the whole game section to wait
+for clicks, assign selected classes and look for matches*/
+game.addEventListener('click', event => {
+
+  const clicked = event.target;
+
+  if (
+    clicked.nodeName === 'SECTION' ||
+    clicked === previousButton ||
+    clicked.parentNode.classList.contains('selected') ||
+    clicked.parentNode.classList.contains('match')
+  ) {
+    return;
+  }
+
+  if (clicksCount < 2) {
+    clicksCount++;
+    if (clicksCount === 1) {
+      firstGuess = clicked.parentNode.dataset.name;
+      console.log(firstGuess);
+      clicked.parentNode.classList.add('selected');
+    } else {
+      secondGuess = clicked.parentNode.dataset.name;
+      console.log(secondGuess);
+      clicked.parentNode.classList.add('selected');
+    }
+
+    if (firstGuess && secondGuess) {
+      if (firstGuess === secondGuess) {
+        setTimeout(match, delay);
+      }
+      setTimeout(resetGuesses, delay);
+    }
+    previousButton = clicked;
+  }
+
 });
